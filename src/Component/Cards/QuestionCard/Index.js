d225f7f6dafb46f9f5916f7f64d84874131ed8f2
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-10-08 21:05:25 +0530

    rectifies question toggle timing
    rectifies question toggle timing

--- src/Component/Cards/QuestionCard/Index.js
+++ src/Component/Cards/QuestionCard/Index.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Index.css';
 import ArrowDown from '../../../Assets/arrowDown.png';
 
 const QuestionCard = ({ question, answer, number }) => {
   const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+  const answerRef = useRef(null);
 
   const toggleAnswerVisibility = () => {
     setIsAnswerVisible(!isAnswerVisible);
   };
 
+  const answerHeight = isAnswerVisible ? `${answerRef.current.scrollHeight}px` : '0';
+
   return (
-    <div className={`parent font-poppins p-1 questionContainer ${isAnswerVisible ? 'border border-pink' : 'border border-white'} flex flex-col rounded-lg`}>
+    <div
+      onClick={toggleAnswerVisibility}
+      className={`parent font-poppins p-1 questionContainer ${
+        isAnswerVisible ? 'border border-pink' : 'border border-white'
+      } flex flex-col rounded-lg`}
+    >
       <div className='flex flex-row justify-between text-white'>
         <div className='flex flex-row'>
-          <div className='serialNo flex flex-col justify-center pl-2 pr-2'>{number+'.'}</div>
+          <div className='serialNo flex flex-col justify-center pl-2 pr-2'>{number + '.'}</div>
           <div className='question flex flex-col justify-center text-lg font-medium '>{question}</div>
         </div>
-        <div className='clickMe' onClick={toggleAnswerVisibility}>
-          <img className='flex flex-col justify-center' src={ArrowDown}/>
+        <div className='clickMe'>
+          <img className='flex flex-col justify-center' src={ArrowDown} />
         </div>
       </div>
-      {isAnswerVisible && (
-        <div className='answer text-white pl-4 text-start pt-4'>{answer}</div>
-      )}
+      <div
+        ref={answerRef}
+        className='answer text-white pl-4 text-start pt-4'
+        style={{ maxHeight: answerHeight, overflow: 'hidden', transition: 'max-height 0.3s ease-in-out' }}
+      >
+        {answer}
+      </div>
     </div>
   );
 };
