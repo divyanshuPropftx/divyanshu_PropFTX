8b4098b123938c3c39f71a6951ed61cca0c4e1cc
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-10-09 02:18:08 +0530

    rectifies shaky card
    rectifies shaky card

--- src/Component/Cards/TeamCard/Index.js
+++ src/Component/Cards/TeamCard/Index.js
@@ -11,7 +11,7 @@ const TeamCard = ({ imageUrl, name, position }) => {
           <img src={imageUrl} />
           <div className='image -mt-14 flex flex-row justify-between pl-4 '>
             <div className='flex flex-col'>
-            <div className='name text-white font-bold text-xl flex flex-col justify-center text-start whitespace-nowrap'>{name}</div>
+            <div className='name text-white font-bold text-xl flex flex-col justify-center text-start'>{name}</div>
               <div className='position  text-white text-xs text-start font-semibold whitespace-nowrap'>{position}</div>
             </div>
             <div className='linkedIn pt-2 mr-4'><img src={LinkedIn} alt='alt' /></div>
