81cb51851ea5cb0b96653c3015018ffb7ca5dfa4
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-10-12 04:11:32 +0530

    adds job opening feature in careers page
    adds job opening feature in careers page

---
+++ src/Component/Cards/CareerCards/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Index.css'
+
+function CareerCard({opening}) {
+  return (
+    <div className='font-poppins'>
+      <div className='careerCard  flex flex-col pt-4 pb-4 pl-6 pr-6  gap-4'>
+        <div className='openingName  text-white text-lg font-semibold '>{opening}</div>
+        <div className='text-lg text-white font-semibold'>View Opportunity</div>
+      </div>
+    </div>
+  )
+}
+
+export default CareerCard
--- src/Component/Careers/Index.js
+++ src/Component/Careers/Index.js
@@ -5,9 +5,37 @@ import Navbar from '../Navbar/Index'
 import Footer from '../Footer/Index'
 import heroCareers from '../../Assets/heroCareers.png'
 import SmallArrowRight from '../../Assets/smallArrowRight.png'
+import CareerCard from '../Cards/CareerCards/Index'
 function Careers() {
+    const [option, setOption] = useState('marketing');
+
     const [selectedOption, setSelectedOption] = useState('marketing');
+
+    // Create an array of job openings with key-value pairs
+    const jobOpenings = [
+      { field: 'marketing', opening: 'SEO Content Writer' },
+      { field: 'marketing', opening: 'Marketing Strategy' },
+      { field: 'marketing', opening: 'SEO Content Writer' },
+      { field: 'marketing', opening: 'Marketing Strategy' },
+      { field: 'marketing', opening: 'Marketing Strategy II' },
+      { field: 'design', opening: 'Product Designer' },
+      { field: 'design', opening: 'Product Designer II' },
+      { field: 'sales', opening: 'Sales Strategy Head' },
+      { field: 'sales', opening: 'Sales Strategy Head II' },
+      { field: 'business', opening: 'Business Development Trainer' },
+      { field: 'business', opening: 'Business Development Trainer II' },
+      { field: 'technology', opening: 'Backend Developer' },
+      { field: 'technology', opening: 'Frontend Developer' },
+      { field: 'technology', opening: 'DevOps Engineer' },
+    ];
+  
+    // Filter job openings based on the selected job field
+    const filteredOpenings = jobOpenings.filter(
+      (opening) => opening.field === selectedOption
+    );
+  
   return (
+
     <div className='parent font-poppins'>
       <div><Navbar/></div>
 
@@ -37,30 +65,38 @@ function Careers() {
         </div>
       </div>
 
-      <div className='flex flex-row justify-center pt-12 pb-12'>
+      <div className='flex flex-row justify-center pt-12 pb-8'>
       <div className='navigateCareer  flex flex-col'> 
             <div className=' flex flex-row justify-between text-white'>
 
-                <div className='positionsCareer' onClick={() => setSelectedOption('marketing')}>Marketing</div>
-                <div className='positionsCareer' onClick={() => setSelectedOption('design')}>Product Designer</div>
-                <div className='positionsCareer' onClick={() => setSelectedOption('sales')}>Sales</div>
-                <div className='positionsCareer whitespace-nowrap' onClick={() => setSelectedOption('business')}>Business Development</div>
-                <div className='positionsCareer' onClick={() => setSelectedOption('technology')}>Technology</div>
+                <div className='positionsCareer' onClick={() => {setOption('marketing'); setSelectedOption('marketing')}}>Marketing</div>
+                <div className='positionsCareer' onClick={() => {setOption('design'); setSelectedOption('design');}}>Product Designer</div>
+                <div className='positionsCareer' onClick={() => {setOption('sales'); setSelectedOption('sales');}}>Sales</div>
+                <div className='positionsCareer whitespace-nowrap' onClick={() => {setOption('business');setSelectedOption('business')}}>Business Development</div>
+                <div className='positionsCareer' onClick={() => {setOption('technology');setSelectedOption('technology');}}>Technology</div>
             </div>
             <div className='h-4 flex flex-col justify-center p-4'><hr></hr></div>
             <div className=' flex flex-row justify-between -mt-5'>
-                <div className={`colorCareers ${selectedOption === 'marketing' ? 'careerColor' : ''} horizontalLines `}>s</div>
-                <div className={`colorCareers ${selectedOption === 'design' ? 'careerColor' : ''} horizontalLines `}>s</div>
-                <div className={`colorCareers ${selectedOption === 'sales' ? 'careerColor' : ''} horizontalLines `}>s</div>
-                <div className={`colorCareers ${selectedOption === 'business' ? 'careerColor' : ''} horizontalLines `}>s</div>
-                <div className={`colorCareers ${selectedOption === 'technology' ? 'careerColor' : ''} horizontalLines `}>s</div>
+                <div className={`colorCareers ${option === 'marketing' ? 'careerColor' : ''} horizontalLines `}>s</div>
+                <div className={`colorCareers ${option === 'design' ? 'careerColor' : ''} horizontalLines `}>s</div>
+                <div className={`colorCareers ${option === 'sales' ? 'careerColor' : ''} horizontalLines `}>s</div>
+                <div className={`colorCareers ${option === 'business' ? 'careerColor' : ''} horizontalLines `}>s</div>
+                <div className={`colorCareers ${option === 'technology' ? 'careerColor' : ''} horizontalLines `}>s</div>
             </div>
       </div>
       </div>
 
+      <div className='flex flex-row justify-center'>
+      <div className='openingsContainer gap-6 flex flex-row flex-wrap'>
+        {filteredOpenings.map((opening, index) => (
+          <CareerCard key={index} opening={opening.opening} />
+        ))}
+      </div>
+    </div>
 
 
     </div>
+
   )
 }
 
